639896702a2f8f9a713439c3a8b9ec7811f83f6f
Sindhu <sponnam@ncsu.edu>
2017-10-24 16:21:31 -0400

    bot integration

--- bot/bot.js
+++ bot/bot.js
@@ -1,4 +1,6 @@
-if (!process.env.token) {
+// https://hooks.slack.com/services/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  : webhook url
+// https://hooks.slack.com/services/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
+if (!process.env.SLACKTOKEN) {
     console.log('Error: Specify token in environment');
     process.exit(1);
 }
@@ -12,63 +14,57 @@ var controller = Botkit.slackbot({
 });
 
 // connect the bot to a stream of messages
-controller.spawn({
-  token: process.env.SLACKTOKEN,
+var bot = controller.spawn({
+  token: process.env.SLACKTOKEN
 }).startRTM()
 
 // Greetings
 controller.hears(['hi', 'hello', 'greetings'],['mention', 'direct_mention','direct_message'], function(bot,message) 
 {
-  console.log(message); 
-  bot.reply(message, "Greetings ");
+  controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello.');
+        }
+    });
 });
 
 controller.hears(/\bhow.*ur.*day.*\b/,['mention', 'direct_mention','direct_message'], function(bot,message) 
-{
-  console.log(message); 
-  var reply = processMessage(message);
+{ 
   bot.reply(message, "Great.! I hope yours is going fine as well.! ");
 });
 
 //Show all pull requests
 controller.hears(/\bpull.*request.*\b/,['mention', 'direct_mention','direct_message'], function(bot,message) 
 {
-  console.log(message); 
-  var pull_reqs = listPullRequests();
-  
-  var reply = processMessage(message);
+  console.log(message);
   bot.reply(message, reply);
 });
 
-//
+
+// Returns list of pull requests to user for a repo.
+controller.hears('List pull requests for octat for repo Hello-World',['mention', 'direct_mention','direct_message'], function(bot,message) 
+{ var repo = "Hello-World"
+  var 
+  var pull_reqs = listPullRequests();
+  
+  bot.reply(message, "Great.! I hope yours is going fine as well.! ");
+});
 
 
-function processMessage(message) {
-	return "reply";
-}
-
-function checkRepo(repo){
-}
-
-function createwebhook(){
-	var create_hook = nock("https://api.github.com")
-      .post("/repos/repos/octocat/Hello-World/hooks", {
-  "name": "web",
-  "active": true,
-  "events": [
-    "push",
-    "pull_request"
-  ],
-  "config": {
-    "url": "http://example.com/webhook",
-    "content_type": "json"
-  }
-})
-      .reply(201, JSON.stringify(data.webhook) );
-}
-
+//pull_request payload
 function getPayLoad(){
 	var payload = data.pull_request_payload;
+}
+
+//Get a pull request info. and send to slack through webhook.
+function getPullRequest(repo, number){
+	var pull_request = nock("https://api.github.com")
+      .get("/repos/octocat/Hello-World/pulls/" + number)
+      .reply(200, JSON.stringify(data.pull_req));
+	  
+	return pull_request; 
 }
 
 // Lists all pull requests of a repo
