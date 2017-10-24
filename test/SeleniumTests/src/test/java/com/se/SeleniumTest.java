ac9262669035ea96723afebd334c98d795251ea8
Sumit Srivastava <ssrivas8@ncsu.edu>
2017-10-24 15:56:20 -0400

    added test cases

--- test/SeleniumTests/src/test/java/com/se/SeleniumTest.java
+++ test/SeleniumTests/src/test/java/com/se/SeleniumTest.java
@@ -21,32 +21,30 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 
+/**
+ * Selenium test class for testing the functionality of CiCdBot
+ * 
+ * @author srivassumit
+ *
+ */
 public class SeleniumTest {
 	private static final String USERNAME = System.getenv("SLACKXUSER");
 	private static final String PASSWORD = System.getenv("SLACKXPWD");
 	private static WebDriver driver;
 
+	/**
+	 * set up
+	 * 
+	 * @throws Exception
+	 */
 	@BeforeClass
 	public static void setUp() throws Exception {
 		ChromeDriverManager.getInstance().setup();
 		driver = new ChromeDriver();
-	}
-
-	@AfterClass
-	public static void tearDown() throws Exception {
-		driver.close();
-		driver.quit();
-	}
-
-	/**
-	 * test case to test that post bot is active and receiving messages.
-	 */
-	@Test
-	public void postMessage() {
 		if (USERNAME == null || PASSWORD == null) {
 			System.out.println(
 					"Set the environment variables SLACKXUSER and SLACKXPWD with the username and password of the slack user to test.");
-			assertTrue(false);
+			throw new Exception("Environment variables not set!");
 		} else {
 			driver.get("https://se-project2017.slack.com/");
 
@@ -65,48 +63,69 @@ public class SeleniumTest {
 			// Click
 			WebElement signin = driver.findElement(By.id("signin_btn"));
 			signin.click();
+		}
+	}
 
-			// Wait until we go to general channel.
-			wait.until(ExpectedConditions.titleContains("general"));
+	/**
+	 * tear down
+	 * 
+	 * @throws Exception
+	 */
+	@AfterClass
+	public static void tearDown() throws Exception {
+		driver.close();
+		driver.quit();
+	}
 
-			// Switch to #selenium-bot channel and wait for it to load.
-			driver.get("https://se-project2017.slack.com/messages/selenium-bot");
-			wait.until(ExpectedConditions.titleContains("selenium-bot"));
+	/**
+	 * Test case to check that the channel is active and receiving messages.
+	 */
+	@Test
+	public void testChannelActive() {
+		driver.get("https://se-project2017.slack.com/messages/selenium-bot");
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.titleContains("selenium-bot"));
 
-			// Type something
-			WebElement messageBot = driver.findElement(By.id("msg_input"));
-			assertNotNull(messageBot);
+		// Type something
+		WebElement messageBox = driver.findElement(By.id("msg_input"));
+		assertNotNull(messageBox);
 
-			Actions actions = new Actions(driver);
-			actions.moveToElement(messageBot);
-			actions.click();
-			actions.sendKeys("hello world, from Selenium");
-			actions.sendKeys(Keys.RETURN);
-			actions.build().perform();
+		Actions actions = new Actions(driver);
+		actions.moveToElement(messageBox);
+		actions.click();
+		actions.sendKeys("hello world, from Selenium");
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
 
-			wait.withTimeout(5, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+		wait.withTimeout(3, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
 
-			WebElement msg = driver
-					.findElement(By.xpath("//span[@class='message_body' and text() = 'hello world, from Selenium']"));
-			assertNotNull(msg);
+		WebElement msg = driver
+				.findElement(By.xpath("//span[@class='message_body' and text() = 'hello world, from Selenium']"));
+		assertNotNull(msg);
+	}
 
-			// WebDriverWait wait = new WebDriverWait(driver, 60);
-			wait.until(ExpectedConditions
-					.visibilityOfElementLocated(By.xpath("//div[@class='p-channel_sidebar__close_container']/a")));
-			List<WebElement> spans = driver
-					.findElements(By.xpath("//div[@class='p-channel_sidebar__close_container']/a"));
-			assertNotNull(spans);
-			// String elementText = spans.get(5).getAttribute("innerText");
+	/**
+	 * test case to test that bot is active.
+	 */
+	@Test
+	public void testBotActive() {
+		driver.get("https://se-project2017.slack.com/messages/selenium-bot");
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.titleContains("selenium-bot"));
 
-			for (int i = 0; i < spans.size(); i++) {
-				String elementText = spans.get(i).getAttribute("innerText");
-				if ("cicdbot".equals(elementText)) {
-					System.out.println(elementText);
-					String status = spans.get(i).getAttribute("innerHTML");
-					System.out.println(status);
-					// assertTrue(status.contains("presence--away"));
-					assertTrue(status.contains("presence--active"));
-				}
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//div[@class='p-channel_sidebar__close_container']/a")));
+		List<WebElement> spans = driver.findElements(By.xpath("//div[@class='p-channel_sidebar__close_container']/a"));
+		assertNotNull(spans);
+
+		for (int i = 0; i < spans.size(); i++) {
+			String elementText = spans.get(i).getAttribute("innerText");
+			if ("cicdbot".equals(elementText)) {
+				System.out.println(elementText);
+				String status = spans.get(i).getAttribute("innerHTML");
+				System.out.println(status);
+				// assertTrue(status.contains("presence--away"));
+				assertTrue(status.contains("presence--active"));
 			}
 		}
 	}
@@ -116,73 +135,23 @@ public class SeleniumTest {
 	 */
 	@Test
 	public void testBotReplies() {
-		if (USERNAME == null || PASSWORD == null) {
-			System.out.println(
-					"Set the environment variables SLACKXUSER and SLACKXPWD with the username and password of the slack user to test.");
-			assertTrue(false);
-		} else {
-			driver.get("https://se-project2017.slack.com/");
+		driver.get("https://se-project2017.slack.com/messages/selenium-bot");
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.titleContains("selenium-bot"));
 
-			// Wait until page loads and we can see a sign in button.
-			WebDriverWait wait = new WebDriverWait(driver, 30);
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signin_btn")));
+		// Type something
+		WebElement messageBot = driver.findElement(By.id("msg_input"));
+		assertNotNull(messageBot);
 
-			// Find email and password fields.
-			WebElement email = driver.findElement(By.id("email"));
-			WebElement pw = driver.findElement(By.id("password"));
+		Actions actions = new Actions(driver);
+		actions.moveToElement(messageBot);
+		actions.click();
+		actions.sendKeys("@cicdbot hello");
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
 
-			// Enter our email and password
-			email.sendKeys(USERNAME);
-			pw.sendKeys(PASSWORD);
-
-			// Click
-			WebElement signin = driver.findElement(By.id("signin_btn"));
-			signin.click();
-
-			// Wait until we go to general channel.
-			wait.until(ExpectedConditions.titleContains("general"));
-
-			// Switch to #selenium-bot channel and wait for it to load.
-			driver.get("https://se-project2017.slack.com/messages/selenium-bot");
-			wait.until(ExpectedConditions.titleContains("selenium-bot"));
-
-			// Type something
-			WebElement messageBot = driver.findElement(By.id("msg_input"));
-			assertNotNull(messageBot);
-
-			Actions actions = new Actions(driver);
-			actions.moveToElement(messageBot);
-			actions.click();
-			actions.sendKeys("@cicdbot hello");
-			actions.sendKeys(Keys.RETURN);
-			actions.build().perform();
-
-			wait.withTimeout(5, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
-			assertTrue(true);
-			// WebElement msg = driver
-			// .findElement(By.xpath("//span[@class='message_body' and text() = 'hello
-			// world, from Selenium']"));
-			// assertNotNull(msg);
-			//
-			// // WebDriverWait wait = new WebDriverWait(driver, 60);
-			// wait.until(ExpectedConditions
-			// .visibilityOfElementLocated(By.xpath("//div[@class='p-channel_sidebar__close_container']/a")));
-			// List<WebElement> spans = driver
-			// .findElements(By.xpath("//div[@class='p-channel_sidebar__close_container']/a"));
-			// assertNotNull(spans);
-			// // String elementText = spans.get(5).getAttribute("innerText");
-			//
-			// for (int i = 0; i < spans.size(); i++) {
-			// String elementText = spans.get(i).getAttribute("innerText");
-			// if ("bleh".equals(elementText)) {
-			// System.out.println(elementText);
-			// String status = spans.get(i).getAttribute("innerHTML");
-			// System.out.println(status);
-			//// assertTrue(status.contains("presence--away"));
-			// assertTrue(status.contains("presence--active"));
-			// }
-			// }
-		}
-
+		wait.withTimeout(3, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+		WebElement msg = driver.findElement(By.xpath("//span[@class='message_body' and text() = 'Greetings']"));
+		assertNotNull(msg);
 	}
 }
