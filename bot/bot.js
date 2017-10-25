a75c035cfd50d2691804b7df447029b59cc51cb3
Sindhu <sponnam@ncsu.edu>
2017-10-24 20:54:50 -0400

    usecases

--- bot/bot.js
+++ bot/bot.js
@@ -118,8 +118,8 @@ function getPullRequest(repo, number) {
 
 // Lists all pull requests of a repo
 function listPullRequests() {
-  var pull_requests = nock("https://api.github.com").get("/repos/octocat/Hello-World/pulls").reply(200, JSON.stringify(data.pull_requests[0]));
-	return data.pull_requests;
+  var pull_requests = nock("https://api.github.com").get("/repos/octocat/Hello-World/pulls").reply(200, JSON.stringify(data.pull_requests));
+	return pull_requests;
 }
 
 // List files of a specific pullrequest
