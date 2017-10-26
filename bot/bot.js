9a001d813e9e0c802d86cb909d5366bbdccd433a
Sindhu <sponnam@ncsu.edu>
2017-10-26 00:05:21 -0400

    refactor

--- bot/bot.js
+++ bot/bot.js
@@ -63,9 +63,7 @@ controller.hears(/\bmerge.*pull.*request.*\b/, [ 'mention', 'direct_mention', 'd
 controller.hears('Get pull requests for octat for repo Hello-World',['mention', 'direct_mention','direct_message'], function(bot,message) 
 { 	var repo = "Hello-World"
 	var result = [];
-	//console.log(listPullRequests().interceptors[0].body)
 	var pull_reqs = JSON.parse(listPullRequests().interceptors[0].body);
-	//console.log(pull_reqs)
 	for(i=0;i<pull_reqs.length;i++){
 	  result.push({Id:pull_reqs[i].id,title:pull_reqs[i].title});
 	}
