eb1517ef698b17c8b06451e82c02eb900f04c31b
Sindhu <sponnam@ncsu.edu>
2017-11-01 22:20:55 -0400

    github integration

--- bot/bot.js
+++ bot/bot.js
@@ -5,7 +5,7 @@ var Table = require('easy-table')
 // Load mock data
 var data = require("./mock.json")
 var Promise = require("bluebird");
-var github = require("./github.js");
+var github = require("./git.js");
 
 if (!process.env.SLACKTOKEN) {
 	console.log('Error: Specify token in environment');
@@ -32,15 +32,58 @@ controller.hears([ 'hi' ], [ 'mention', 'direct_mention', 'direct_message' ], fu
 // Details of a particular pull request
 controller.hears('Get pull request 1 for octat for repo Hello-World',['mention', 'direct_mention','direct_message'], function(bot,message) 
 { 	var repo = "Hello-World"
-	console.log(getPullRequest())
-	var pull_req = JSON.parse(getPullRequest().interceptors[0].body);
-	var t = new Table
-	t.cell('Id', pull_req.id)
-	t.cell('State	', pull_req.state)
-	t.cell('Title	', pull_req.title)
-	t.cell('Description	', pull_req.body)
-	t.newRow()
-    bot.reply(message, t.toString());
+	var owner = "octat"
+	var number = 1
+	var pull_req = github.getPullRequest(owner, repo, number)
+	pull_req.then(function(value){
+		var t = new Table
+		t.cell('Id', value.id)
+		t.cell('State	', value.state)
+		t.cell('Title	', value.title)
+		t.cell('Description	', value.body)
+		t.newRow()
+		bot.reply(message, t.toString());
+	});
+	// var pull_req = JSON.parse(getPullRequest().interceptors[0].body);
+	// var t = new Table
+	// t.cell('Id', pull_req.id)
+	// t.cell('State	', pull_req.state)
+	// t.cell('Title	', pull_req.title)
+	// t.cell('Description	', pull_req.body)
+	// t.newRow()
+    // bot.reply(message, t.toString());
+});
+
+controller.hears('Get pull requests for octat for repo Hello-World',['mention', 'direct_mention','direct_message'], function(bot,message) 
+{ 	var repo = "Hello-World"
+	var owner = "octat"
+	var pull_reqs = github.listPullRequests(owner, repo)
+	pull_reqs.then(function(value){
+		var result = [];
+		for(i=0;i<value.length;i++){
+			result.push({Id:value[i].id,title:value[i].title});
+		}
+		var t = new Table
+		result.forEach(function(req) {
+		t.cell('Id', req.Id)
+		t.cell('Title	', req.title)
+		t.newRow()
+		})
+		bot.reply(message, t.toString());
+	});
+	// var result = [];
+	// var pull_reqs = JSON.parse(listPullRequests().interceptors[0].body);
+	// for(i=0;i<pull_reqs.length;i++){
+	  // result.push({Id:pull_reqs[i].id,title:pull_reqs[i].title});
+	// }
+	// var t = new Table
+
+	// result.forEach(function(req) {
+	// t.cell('Id', req.Id)
+	// t.cell('Title	', req.title)
+	// t.newRow()
+	// })
+  // bot.reply(message, t.toString());
 });
 
 //@botCiCd merge #1 pull request for aakarshg/serverprovision
@@ -64,23 +107,6 @@ controller.hears(/\bmerge.*pull.*request.*\b/, [ 'mention', 'direct_mention', 'd
 });
 
 
-controller.hears('Get pull requests for octat for repo Hello-World',['mention', 'direct_mention','direct_message'], function(bot,message) 
-{ 	var repo = "Hello-World"
-	var result = [];
-	var pull_reqs = JSON.parse(listPullRequests().interceptors[0].body);
-	for(i=0;i<pull_reqs.length;i++){
-	  result.push({Id:pull_reqs[i].id,title:pull_reqs[i].title});
-	}
-	var t = new Table
-
-	result.forEach(function(req) {
-	t.cell('Id', req.Id)
-	t.cell('Title	', req.title)
-	t.newRow()
-	})
-  bot.reply(message, t.toString());
-});
-
 // Getting the details from jenkins and this is where bot is supposed to hit git's rest api to get all details.
 controller.hears(/\bsample.*Pull.*request.*submitted\b/,['mention', 'direct_mention','direct_message'], function(bot,message) 
 {    console.log("Got the message");
@@ -89,17 +115,6 @@ controller.hears(/\bsample.*Pull.*request.*submitted\b/,['mention', 'direct_ment
    channel: 'selenium-test'
     });
 });
-
-// // Returns list of pull requests to user for a repo.
-// controller.hears('List pull requests for octat for repo Hello-World', 
-// ['mention', 'direct_mention', 'direct_message' ], function(bot, message) {
-//     var repo = "Hello-World"
-//     console.log('inside octocat');
-//     var pull_req = listPullRequests();//PRNumber: 1347, title: new-feature
-//     var msg = "PRNumber: " + pull_req.number+ ", title: " + pull_req.title
-//     bot.reply(message, msg);
-// });
-
 
 // pull_request payload
 function getPayLoad() {
