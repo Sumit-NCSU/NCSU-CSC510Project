a942b5f50b92d2bee7f09cd6434fcf205d021777
Aakarsh Gopi <agopi@ncsu.edu>
2017-11-14 22:25:33 -0500

    Updated final version bot.js
    This is the final version of bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -4,6 +4,8 @@ var Table = require('easy-table')
 var Promise = require("bluebird");
 var github = require("./gitinterface.js");
 
+
+
 if (!process.env.SLACKTOKEN) {
 	console.log('Error: Specify Git token in environment variable: SLACKTOKEN');
 	process.exit(1);
@@ -30,17 +32,44 @@ controller.configureSlackApp({
 controller.hears(['hi'], [ 'mention', 'direct_mention', 'direct_message' ], function(bot, message) {
 	controller.storage.users.get(message.user, function(err, user) {
     	console.log('inside hi');
-		bot.reply(message, 'Hello');
+		bot.reply(message, 'Sample');
 	});
 });
 
+
+controller.hears(/\bissue.*request.*\b/,['mention', 'direct_mention','direct_message'], function(bot,message) {
+	// bot says: issue pull request on aakarshg/Serverprovision aakarshg-patch-3
+
+	var X = "issue"
+	var text_message = message.text
+	var Z = "successfully issued " + text_message.toString().split(X).pop();
+
+
+
+	var repo = "Serverprovision"
+	var owner = "aakarshg"
+	var branchName = "aakarshg-patch-4"
+	var base = "master"
+	var x = github.createPullRequest(owner, repo, branchName,base)
+	if (x==true){
+		console.log("Received response in bot.js")
+	bot.reply(message,Z)
+	}
+});
+
 //TODO: generalize this, implement drop down list in this.
-controller.hears('Get pull requests for octat for repo Hello-World',['mention', 'direct_mention','direct_message'], function(bot,message) {
-	var repo = "SEGitAPI"
-	var owner = "srivassumit"
-	var branchName = "master"
-	var isOpen = true
-	github.getPullRequests(owner, repo, isOpen, branchName, (value) => {
+controller.hears(/\bget.*requests.*\b/,['mention', 'direct_mention','direct_message'], function(bot,message) {
+	// bot says: Get pull requests for octat for repo Hello-World
+	/*
+
+	var sample_reply = "In the get pull requests"
+	bot.reply(message,sample_reply.toString())
+
+	*/
+
+	var repo = "Serverprovision"
+	var owner = "aakarshg"
+	github.getPullRequests(owner, repo, (value) => {
 		console.log('Bot: Received ' + value.length + ' Pull requests');
 		var result = [];
 		for(i=0;i<value.length;i++){
@@ -54,41 +83,53 @@ controller.hears('Get pull requests for octat for repo Hello-World',['mention', 
 		})
     	bot.reply(message, t.toString());
 	});
+
 });
 
 //TODO: generalize this, something like this should also be called when user selects a PR from the drop down for all PRs.
-controller.hears('Get pull request 1 for octat for repo Hello-World',['mention', 'direct_mention','direct_message'], function(bot,message) { 	
+controller.hears(/\bget.*request.*\b/,['mention', 'direct_mention','direct_message'], function(bot,message) {
+	// let the bot say: Get pull request 1 for octat for repo Hello-World
 	//TODO: remove this?
+	/*
+	var sample_reply = "In the get pull request number"
+	bot.reply(message,sample_reply.toString())
+	*/
+	/*
 	bot.startConversation(message, function(err, convo) {
 		convo.say('Better take this private...')
 		convo.say({ ephemeral: true, text: 'These violent delights have violent ends' })
 	})
-	var repo = "Hello-World" // extract this from user message/intent/context?
-	var owner = "octat" // extract this from user message/intent/context?
-	var number = 1 // extract this from user message/intent/context?
-	var branchName = "" // extract this from user message/intent/context?
-	var pull_req = github.getPullRequest(owner, repo, number, branchName)
-	var t = new Table
-	t.cell('Id', pull_req.id)
-	t.cell('State	', pull_req.state)
-	t.cell('Title	', pull_req.title)
-	t.cell('Description	', pull_req.body)
-	t.newRow()
-    bot.reply(message, t.toString());
+	*/
+	var repo = "Serverprovision" // extract this from user message/intent/context?
+	var owner = "aakarshg" // extract this from user message/intent/context?
+	var number = 15 // extract this from user message/intent/context?
+	var branchName = "master" // extract this from user message/intent/context?
+	github.getPullRequest(owner, repo,number, (value) => {
+		console.log(value)
+		var t ="Id: " + value.id + " Title: " +value.title + "description: " +value.body
+    	bot.reply(message, t.toString());
+	});
+
 });
 //@botCiCd merge #1 pull request for aakarshg/serverprovision
 //TODO: generalize this, pull pr number etc. from message.
-controller.hears(/\bmerge.*pull.*request.*\b/, [ 'mention', 'direct_mention', 'direct_message' ], function(bot, message) {
+
+controller.hears(/\bmerge.*\b/, [ 'mention', 'direct_mention', 'direct_message' ], function(bot, message) {
+	// old regex: \bmerge.*pull.*request.*\b
 	// TODO: Aakarsh to do the Jenkins integration for merging request. U can put your code here fro merging pull request.
 	console.log(message);
 	console.log('inside pr merge');
-	var repo = "SEGitAPI" // extract this from user message/intent/context?
-	var owner = "srivassumit"// extract this from user message/intent/context?
-	var prnumber = 10;// extract this from user message
-	var adminlist = ["aakarshg", "assinsin", "sebotcicd","U6WGAURSQ","U6VUKPYCR"];
+	var repo = "serverprovision" // extract this from user message/intent/context?
+	var owner = "aakarshg"// extract this from user message/intent/context?
+	var prnumber = 15;// extract this from user message
+	var branch = "aakarshg-patch-4"
+	var adminlist = ["U6WCFDZL3", "assinsin", "sebotcicd","U6WGAURSQ","U6VUKPYCR"];
 	var reply = '';
 	//check admin list before actually merging
 	if(adminlist.indexOf(message.user) > -1) {
+	github.getStatus(owner,repo,branch,(out) =>{
+	if(out)	{
+
 		github.mergePullRequest(owner, repo, prnumber, (msg) => {
 			if (msg) {
 				console.log('msg received in bot: ' + msg)
@@ -96,7 +137,15 @@ controller.hears(/\bmerge.*pull.*request.*\b/, [ 'mention', 'direct_mention', 'd
 				bot.reply(message, reply);
 			}
 		});
-	} else {
+	}
+else{
+	reply = "Build has been unsuccessful";
+	bot.reply(message, reply);
+}
+});
+
+}
+	else {
 		reply = "You don't have permission to merge through the bot interface!";
 		bot.reply(message, reply);
 	}
