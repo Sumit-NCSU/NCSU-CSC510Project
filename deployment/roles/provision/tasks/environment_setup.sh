1d707eddc7c1e5b8f080501c2761ebaafcf2c42d
asinsinwal <assinsin@ncsu.edu>
2017-11-27 18:59:31 -0500

    Instruction for keys

--- deployment/roles/provision/tasks/environment_setup.sh
+++ deployment/roles/provision/tasks/environment_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "export SLACKTOKEN="xoxb-277566300433-LVNMFkqkh4u6s1O70BKUgPNh"" >> /etc/environment
-echo "export CIBOTCID="234420262803.278786608823"" >> /etc/environment
-echo "export CIBOTCSEC="8e238a7b0cca46530ad840104a3fbb47"" >> /etc/environment
-echo "export GITTOKEN="18550fc0536fba882309f170d537b48c116f03a6"" >> /etc/environment
+echo "export SLACKTOKEN="YOUR_SLACKTOKEN_GOES_HERE"" >> /etc/environment
+echo "export CIBOTCID="YOUR_BOT_CLIENT_ID_GOES_HERE"" >> /etc/environment
+echo "export CIBOTCSEC="YOUR_BOT_CLIENT_SECRET_GOES_HERE"" >> /etc/environment
+echo "export GITTOKEN="YOUR_GITTOKEN_GOES_HERE"" >> /etc/environment
 
 source ~/.bashrc
